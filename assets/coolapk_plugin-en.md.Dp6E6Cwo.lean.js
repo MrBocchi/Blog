8d24433ec71664dfcc773d6215fdce3e624e1d61
Mr_Bocchi <1301371460@qq.com>
2026-01-01 23:25:10 +0800

    Update 2026-1-1 post

---
+++ assets/coolapk_plugin-en.md.Dp6E6Cwo.lean.js
@@ -0,0 +1,17 @@
+import{_ as s,c as o,o as r,a6 as t,j as a,b2 as d,b3 as n,b4 as c,b5 as h,b6 as m,b7 as b,b8 as p,b9 as u,ba as v,bb as g,bc as i,bd as l}from"./chunks/framework.B_0yKDO6.js";const E=JSON.parse('{"title":"🌟 Control Center Master Index","description":"《🌟控制中心汇总贴》的英文版","frontmatter":{"description":"《🌟控制中心汇总贴》的英文版","tag":["酷安"]},"headers":[],"relativePath":"coolapk/plugin-en.md","filePath":"coolapk/plugin-en.md"}'),f={name:"coolapk/plugin-en.md"};function k(y,e,w,S,_,x){return r(),o("div",{"data-pagefind-body":!0},e[0]||(e[0]=[t("",19),a("div",{onclick:"event.stopPropagation()",style:{"border-radius":"12px",overflow:"hidden",display:"inline-block","max-width":"80%"}},[a("img",{src:c,class:"prev-img",onclick:`
+      const img = this;
+      const v=document.createElement('video');
+      v.src='/prev-img/preview_switch_devices.mp4';
+      v.controls=false;
+      v.autoplay=true;
+      v.playsInline=true;
+      v.loop=true;
+      v.muted=true;
+      v.style.maxHeight = '300px';
+      v.onclick = function (e) {
+        e.stopPropagation();
+        v.pause();
+        v.replaceWith(img);
+      };
+      img.replaceWith(v);
+    `})],-1),t("",46)]))}const M=s(f,[["render",k]]);export{E as __pageData,M as default};
---
+++ assets/coolapk_plugin.md.a1MhfrdF.lean.js
@@ -0,0 +1,17 @@
+import{_ as i,c as r,o as d,a6 as t,j as l,be as c,bf as o,b4 as b,b5 as h,b6 as m,b7 as n,b8 as p,b9 as v,ba as g,bb as k,bc as s,bd as e,aX as f,aY as _}from"./chunks/framework.B_0yKDO6.js";const z=JSON.parse('{"title":"🌟控制中心汇总贴","description":"分类整理了本人制作的所有粮厂高仿iOS控制中心。","frontmatter":{"top":2,"sticky":999,"description":"分类整理了本人制作的所有粮厂高仿iOS控制中心。","descriptionHTML":" <span class=\\"description\\" style=\\"display:flex;align-items:center;justify-content:space-between;gap:1rem;\\"> <span style=\\"flex:1;\\"> <b style=\\"font-size:1.1rem;color:#409EFF;\\">控制中心整理贴</b><br> 分类整理了本人制作的所有粮厂高仿iOS控制中心。 </span> <img src=\\"/coolapk-img/controlcenter.png\\" alt=\\"logo\\" style=\\"width:90px;height:90px;border-radius:8px;\\"> </span> ","tag":["酷安"],"recommend":1},"headers":[],"relativePath":"coolapk/plugin.md","filePath":"coolapk/plugin.md"}'),u={name:"coolapk/plugin.md"};function w(O,a,S,M,y,E){return d(),r("div",{"data-pagefind-body":!0},a[0]||(a[0]=[t("",19),l("div",{onclick:"event.stopPropagation()",style:{"border-radius":"12px",overflow:"hidden",display:"inline-block","max-width":"80%"}},[l("img",{src:b,class:"prev-img",onclick:`
+      const img = this;
+      const v=document.createElement('video');
+      v.src='/prev-img/preview_switch_devices.mp4';
+      v.controls=false;
+      v.autoplay=true;
+      v.playsInline=true;
+      v.loop=true;
+      v.muted=true;
+      v.style.maxHeight = '300px';
+      v.onclick = function (e) {
+        e.stopPropagation();
+        v.pause();
+        v.replaceWith(img);
+      };
+      img.replaceWith(v);
+    `})],-1),t("",50)]))}const G=i(u,[["render",w]]);export{z as __pageData,G as default};
